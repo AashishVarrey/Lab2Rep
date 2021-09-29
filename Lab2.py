482f9aba5724e313485878259a21166a62ad8426
Aashish Varrey <91568763+AashishVarrey@users.noreply.github.com>
2021-09-29 01:36:18 +0000

    final plus comments

--- Lab2.py
+++ Lab2.py
@@ -1,25 +1,31 @@
+#import necessary functions
 import RPi.GPIO as GPIO
 from time import sleep
 
+
 GPIO.setmode(GPIO.BCM)
 
+#define input and output port numbers
 led1 = 4
 led2 = 13
 led3 = 12
 topbutton = 24
 bottombutton = 25
 
+#pin setup
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 GPIO.setup(topbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(bottombutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+#variables and constants for pwm objects
 f = 1
 dc = 50
 pwm1 = GPIO.PWM(led1,f)
 pwm2 = GPIO.PWM(led2,f)
 
+#threaded callback function, two for loops, one for brightness one for dimming
 def my_callback(channel):
     if channel == topbutton:
         pwm1.start(0)
@@ -29,7 +35,7 @@ def my_callback(channel):
         for y in range(100,0,-1):
             pwm1.ChangeDutyCycle(y)
             sleep(0.01)
-        print("top")
+        #print("top")
         pwm1.stop()
     if channel == bottombutton:
         pwm2.start(0)
@@ -39,10 +45,10 @@ def my_callback(channel):
         for y in range(100,0,-1):
             pwm2.ChangeDutyCycle(y)
             sleep(0.01)
-        print("bottom")
+        #print("bottom")
         pwm2.stop()
 
-
+#main code, execute callbacks based on which button pushed
 try:
     pwm = GPIO.PWM(led3,f)
     pwm.start(dc)
