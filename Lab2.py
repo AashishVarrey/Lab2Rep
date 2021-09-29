fe685a5b817698e1b9b49487b0bea5f4d6cdef50
Aashish Varrey <91568763+AashishVarrey@users.noreply.github.com>
2021-09-29 01:30:40 +0000

    done

--- Lab2.py
+++ Lab2.py
@@ -3,8 +3,8 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-led1 = 21
-led2 = 26
+led1 = 4
+led2 = 13
 led3 = 12
 topbutton = 24
 bottombutton = 25
@@ -17,36 +17,37 @@ GPIO.setup(bottombutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 f = 1
 dc = 50
+pwm1 = GPIO.PWM(led1,f)
+pwm2 = GPIO.PWM(led2,f)
 
 def my_callback(channel):
-#  pwm23 = GPIO.PWM(channel,f)
-#  pwm23.start(1)
-  if channel == topbutton:
-    GPIO.output(led1,1)
-#    pwm1 = GPIO.PWM(led1,1)
-#    for x in range(101):
-#     pwm1.ChangeDutyCycle(x)
-#   for y in range(101,0,-1):
-#     pwm1.ChangeDutyCycle(y)
-  
-    print("top")
-  if channel == bottombutton:
-    print("bottom")
-#  if GPIO.input(channel) == GPIO.HIGH:
-#  for x in range(101):
-#    pwm.ChangeDutyCycle(x)
-#  for y in range(101,0,-1):
-#    pwm.ChangeDutyCycle(y)
-#  pwm.stop()
-#  else:
-#    pass
+    if channel == topbutton:
+        pwm1.start(0)
+        for x in range(101):
+            pwm1.ChangeDutyCycle(x)
+            sleep(0.01)
+        for y in range(100,0,-1):
+            pwm1.ChangeDutyCycle(y)
+            sleep(0.01)
+        print("top")
+        pwm1.stop()
+    if channel == bottombutton:
+        pwm2.start(0)
+        for x in range(101):
+            pwm2.ChangeDutyCycle(x)
+            sleep(0.01)
+        for y in range(100,0,-1):
+            pwm2.ChangeDutyCycle(y)
+            sleep(0.01)
+        print("bottom")
+        pwm2.stop()
 
-pwm = GPIO.PWM(led3,f)
 
 try:
+    pwm = GPIO.PWM(led3,f)
+    pwm.start(dc)
     GPIO.add_event_detect(topbutton, GPIO.RISING, callback=my_callback)
     GPIO.add_event_detect(bottombutton, GPIO.RISING, callback=my_callback)
-    pwm.start(dc)
     while True:
         pass
 except KeyboardInterrupt:
